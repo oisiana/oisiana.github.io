f146c8bb7556e6e5ffa08d96dc15e15c1f7225a5
oisiana <59921676+oisiana@users.noreply.github.com>
2025-11-25 20:26:56 +0000

    scripts

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-const list = document.createElement('ul');
-const info = document.createElement('p');
-const html = document.querySelector('html');
+// Store a reference to the <h1> in a variable
+const myHeading = document.querySelector("h1");
+// Update the text content of the <h1>
+myHeading.textContent = "Hello world!";
 
-info.textContent = 'Below is a dynamic list. Click anywhere on the page to add a new list item. Click an existing list item to change its text to something else.';
+const myImage = document.querySelector("img");
 
-document.body.appendChild(info);
-document.body.appendChild(list);
-
-html.onclick = function() {
-  const listItem = document.createElement('li');
-  const listContent = prompt('What content do you want the list item to have?');
-  listItem.textContent = listContent;
-  list.appendChild(listItem);
-
-  listItem.onclick = function(e) {
-    e.stopPropagation();
-    const listContent = prompt('Enter new content for your list item');
-    this.textContent = listContent;
+myImage.addEventListener("click", () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
   }
-}
+});
